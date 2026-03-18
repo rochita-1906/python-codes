dc7f8ca0635e9ac5179ad2f55cf1eb30050882bf
Rochita Gowda <rochita1906@gmail.com>
2026-03-18 22:23:25 +0530

    Add swap function using temporary variable

---
+++ swap_with_temp.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+print("Before swapping: a=",a,", b=",b,)
+temp=a
+a=b
+b=temp
+print("After swapping: a=",a,", b=",b,)
